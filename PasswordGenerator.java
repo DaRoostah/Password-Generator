92caa4db082989e4011f3a3a666c49274f5c4694
DaRoostah <smas3971@gmail.com>
2022-04-25 21:44:36 -1000

    Password Generator 1.0 Completed
    Soon too add a text file, create a UI to improve UX. Will be soon improving Password Generator method to be able to incorporate all characters initialized within the String  variables.

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -12,20 +12,27 @@ public class PasswordGenerator {
         System.out.println("How many characters do you want?");
         String userInput = sc.nextLine();
         int characterNumber = Integer.parseInt(userInput);
-        System.out.println(GeneratePassword(characterNumber));
+        System.out.println("How many passwords do you want printed out?");
+        userInput = sc.nextLine();
+        int pwNumber = Integer.parseInt(userInput);
+        for(int numOfPass = 0; numOfPass < pwNumber;numOfPass++) {
+          System.out.println(numOfPass+1 + ". " + GeneratePassword(characterNumber));
+        }
 
     }
     public static String GeneratePassword(int characterNumber) {
         Random rand = new Random();
         String[] characterChooser = new String[4];
+
+        
         characterChooser[0] = lowerCaseCharacters;
         characterChooser[1] = upperCaseCharacters;
         characterChooser[2] = numberCharacters;
         characterChooser[3] = specialCharacters;
         String password = "";
         for(int passwordChar = 0; passwordChar < characterNumber; passwordChar++) {
-            int randNumber = rand.nextInt(4);
-            password += characterChooser[randNumber].substring(rand.nextInt(characterChooser[randNumber].length())), characterChooser[randNumber].substring(rand.nextInt(characterChooser[randNumber].length())+1);
+            int randNumber = rand.nextInt(characterChooser.length), randChar = rand.nextInt(characterChooser[randNumber].length());
+            password += characterChooser[randNumber].substring(randChar, randChar+1 );
         }
         return password;
     }
